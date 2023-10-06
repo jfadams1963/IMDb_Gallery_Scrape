2132dd407286018a275aa86f3d60a9fa2b2fd8d7
Jonathan Adams <jfadams1963@proton.me>
2023-10-06 19:26:42 -0400

    Create imdb_gallery_scrape.py

---
+++ imdb_gallery_scrape.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+"""
+ This is modified from https://github.com/IvRogoz
+ (License ??)
+ The bulk of this code is due to a lot work on the part of
+ IvRogoz (where ever you are!). I and anyone who uses this code
+ owes IvRogoz a big thanks.
+
+ I wish IvRogoz had included a license statement so that I could
+ honor it as I pass this work on. My personal choice is the
+ 2-clause BSD license. If I can ever get in touch whith IvRogoz, I
+ will ask them about it. In the mean time, this work is released
+ for use free of charge or any encumberences. There are no statements
+ of fitness for use, etc., etc. It comes with no warranties, etc.,
+ etc. Use it at your own risk and do what ever you will with it.
+ It's certainly not perfect, and could use a little work...
+ 
+ Modified  Sep/Oct 2023 by https://github.com/jfadams1963:
+ + Made changes to reflect the new structure on IMDb.
+ + Added Cinemagoer functionality.
+ + Modifed requests to go through a scraper proxy to keep IMDb happy.
+   ->Get a free API key at https://scrapeops.io/
+ + Using SoupStrainer for more efficient html parsing.
+ + Using settings for API_KEY (optional)
+ + Usage:
+     imdb_gallery_scrape.py movie <title|movieID>
+     imdb_gallery_scrape.py actor|person <name|personID>
+"""
+
+import sys
+import os
+import string
+import re
+import shutil
+import requests
+from urllib.request import urlopen
+from python_settings import settings
+from bs4 import BeautifulSoup, SoupStrainer
+from imdb import (Cinemagoer,
+                    IMDbError,
+                    IMDbDataAccessError)
+
+
+#### Added by jfadams1963 
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+# You will need an APi key from https://scrapeops.io/
+# Getting API_KEY via settings. Set it however works best for you. -jfadams1963
+API_KEY = settings.SCRAPEOPS_API_KEY
+is_person = 0
+image_num_limit = 20 # Use this to limit number of images downloaded  -jfadams1963
+####
+
+start_page = 0
+paggination = 1
+mediaview_url = "https://www.imdb.com/"
+
+#### Cineamagoer section to get movieID or personID -jfadams1963
+# Set up person|movie options
+# Instantiate a Cinemagoer object
+# Get a movies|persons object
+bad = r'['+string.punctuation+']'
+# Strip input of punctuation, set to lower case
+# IMDb queries are case insensitive
+arg1 = re.sub(bad,'',sys.argv[1]).lower()
+arg2 = re.sub(bad,'',sys.argv[2])
+# Use ID is initially false
+use_id = 0
+person_id = 0
+
+if arg2.isnumeric() == True:
+    use_id = 1
+    person_id = 1
+
+#allow 'actor' for person
+if (arg1 == 'actor') or arg1 == ('person'):
+    arg1 = 'person'
+    is_person = 1
+
+movie = ''
+persons = ''
+try:
+    ia = Cinemagoer()
+    if (use_id == 1) and (is_person == 0):
+        title = ia.get_movie(arg2)['title']
+        movies = ia.search_movie(title)
+    elif (use_id == 0) and (is_person == 0):
+        title = arg2
+        movies = ia.search_movie(arg2)
+
+    if (use_id == 1) and (is_person == 1):
+        nom = ia.get_person(arg2)['name']
+        persons = ia.search_person(nom)
+        first_last = nom.split()
+        name = first_last[0] + '_' + first_last[1]
+
+    elif (use_id == 0) and (is_person == 1):
+        persons = ia.search_person(arg2)
+        first_last = arg2.split()
+        name = first_last[0] + '_' + first_last[1]
+except  (IMDbError, IMDbDataAccessError) as err:
+    print(err)
+    sys.exit("Exception instantiating Cinemagoer")
+
+if is_person == 0:
+    movie_id = movies[0].movieID
+    print('ID',movie_id)
+    imdb_ID = 'tt'+str(movie_id)
+    folder = str(title)
+    image_tag = str(title)
+    base_url = "https://www.imdb.com/title/" + imdb_ID + "/mediaindex/"
+elif is_person == 1:
+    person_id = persons[0].personID
+    imdb_ID = 'nm'+str(person_id)
+    folder = str(name)
+    image_tag = str(name)
+    base_url = "https://www.imdb.com/name/" + imdb_ID + "/mediaindex/"
+#### End Cinemagoer section
+
+
+# Create directory based on title or name -jfadams1963
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+folder = "./" + folder + "/"
+print("Created Directory:", folder)
+
+i = 0 # For enumerating individual images -jfadams1963
+
+# x enumerates gallery pages -jfadams1963
+for x in range(start_page, paggination):
+    if x > 0:
+        url =base_url +"?page="+str(x)
+    else:
+        url = base_url
+    print()
+    print("Scrapping from:", url)
+    
+    ## use requests.get().content with scraper proxy -jfadams1963
+    htmldata = requests.get(url = 'https://proxy.scrapeops.io/v1/',
+                            params = {'api_key': API_KEY,
+                            'url': base_url },
+               timeout = 20).content
+    soup = BeautifulSoup(htmldata, 'html.parser')
+    images = soup.find_all(class_='media_index_thumb_list')
+    links = images[0].find_all('a')
+    print("Found:", len(links), "images")
+    print("Will download " + str(image_num_limit))
+
+    for index, link in enumerate(links):
+        i += 1
+        if i > image_num_limit: # Using a limit on number of images. (optional)
+            sys.exit(0)
+        image_i = link['href']
+        # This url gives us the mediaviewer page with image i -jfadams1963
+        url = mediaview_url + image_i
+        # Use proxy and requests.get().content -jfadams1963
+        # This URL will contain the source URL for the large image,
+        # so let's grab it and parse it -jfadams1963
+        founddata = requests.get(url = 'https://proxy.scrapeops.io/v1/',
+                                params = {'api_key': API_KEY,
+                                          'url': url, },
+                    timeout = 20).content
+        # Here we get our large image URL -jfadasm1963
+        meta_with_image = SoupStrainer(property="og:image")
+        image_source_soup = BeautifulSoup(founddata, 'html.parser', parse_only=meta_with_image)
+        image_url = image_source_soup.find('meta')['content']
+        print('')
+        print('Image no. ' + str(i) + ' of ' + str(image_num_limit))
+
+        # Hmm, need to handle this condition -jfadams1963
+        if image_url is None:
+            print("No image found")
+            continue
+        print("Downloading " + image_url)
+        file_name = folder + image_url.split('/')[-1]
+
+        try:
+            # Use scrapeops proxy to download image  -jfadams1963
+            res = requests.get(url = 'https://proxy.scrapeops.io/v1/',
+                               params = {'api_key': API_KEY,
+                                         'url': image_url, },
+                  stream = True, timeout = 20)
+            
+            exists = False
+            if res.status_code == 200:
+                exists = os.path.isfile(file_name)
+
+            g = 0
+            while exists:
+                print("file exists:", file_name)
+                g += 1
+                file_name =folder+str(index)+"_"+str(g)+"_"+ image_url.split('/')[-1]
+                exists = os.path.isfile(file_name)
+                    
+            with open(file_name,'wb') as f:
+                shutil.copyfileobj(res.raw, f) 
+                # Let's get rid of rediculous file names  -jfadams1963
+                file_name = os.rename(file_name,
+                                      folder + image_tag + '_' + str(i) + '.jpg')
+
+            saved = os.path.isfile(file_name)
+            # Should be =>if saved<=  -jfadams1963
+            #if not saved:
+            if saved:
+                print(">>>> ",saved)
+                print('Image sucessfully Downloaded: ',file_name)
+            else:
+                print('Image Couldn\'t be retrieved')
+
+        except Exception as e:
+            print(e)
